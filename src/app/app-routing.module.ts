f0059afdc358df9b332a2b00572b27cd179a7427
Afaq, Muhammad <muhammad.afaq@accenture.com>
2020-03-18 14:24:07 -0400

    Login page & home minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: AdminLoginComponent },
+  { path: 'home', component: AdminHomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
