6248d872bafb9e5a1856cb30678b99507e1b91cf
Afaq, Muhammad <muhammad.afaq@accenture.com>
2020-03-25 21:27:48 -0400

    Cosmetics for components

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLoginComponent implements OnInit {
 
-  constructor() { }
+  onLoginForm: FormGroup;
+  email: string = '';
+  password: string = '';
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit() {
+    this.onLoginForm = this.fb.group({
+      email: ['', Validators.compose([
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.required
+      ])]
+    })
   }
 
 }
--- src/app/all-categories/all-categories.component.ts
+++ src/app/all-categories/all-categories.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-categories.component.scss']
 })
 export class AllCategoriesComponent implements OnInit {
-  
+  category: any = {};
+  userImage: any = '';
+  newFile: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChangeFile(event: EventTarget) {
+    let eventObj: MSInputMethodContext = <MSInputMethodContext>event;
+    let target: HTMLInputElement = <HTMLInputElement>eventObj.target;
+    let files: FileList = target.files;
+    this.userImage = files[0];  // Object of image containing image properties. for example name, size etc
+    var reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.category.profileUrl = reader.result;
+      this.newFile = true;
+
+    }
   }
 
 }
